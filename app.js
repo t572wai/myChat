73c93a9424021e54fd5374f1b90be163dc9b52e0
t572wai <53143353+t572wai@users.noreply.github.com>
2021-10-03 14:41:15 +0900

    heroku移行

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://mychatwai.herokuapp.com:3000',
   credentials: true,
   optionsSuccessStatus: 200,
   transports: ['websocket']
